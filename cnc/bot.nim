f9439db250407b1c9fa5dbcdb1d29df645eb830d
Timo Sarkar <71646577+0xtimosarkar@users.noreply.github.com>
2023-06-03 16:18:21 +0200

    rewritten bot.go in nim

--- cnc/bot.nim
+++ cnc/bot.nim
@@ -1,5 +1,30 @@
-proc NewBot
+import net
+import times
 
-proc Handle
+type Bot = ref object
+  uid: int
+  conn: Socket
+  version: byte
+  source: string
 
-proc QueueBuf
+proc newBot(conn: Socket, version: byte, source: string): Bot =
+  result = Bot(uid: -1, conn: conn, version: version, source: source)
+
+proc handle(this: Bot) =
+  clientList.addClient(this)
+  finally:
+    clientList.delClient(this)
+  var buf: seq[uint8] = newSeqWithCap[uint8](2)
+  while true:
+    this.conn.setDeadline(now() + seconds(180))
+    let n = this.conn.read(buf)
+    if n.isNil or n.len != buf.len:
+      return
+    if this.conn.write(buf) != buf.len:
+      return
+
+proc queueBuf(this: Bot, buf: seq[uint8]) =
+  this.conn.write(buf)
+
+var clientList: seq[Bot] = @[]
+
